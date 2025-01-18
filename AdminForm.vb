3700fb6a00a3976879bb404cd15710c6c6a2d06a
rizkiismail9a <muhamadrizkiismail9a@gmail.com>
2025-01-18 16:53:00 +0700

    feat: create update and delete feature

--- AdminForm.vb
+++ AdminForm.vb
@@ -25,6 +25,54 @@ Public Class AdminForm
         InputTreeAmount.Text = ""
     End Sub
 
+    Private Sub SearchTree()
+        Try
+            Dim query = $"SELECT * FROM mytrees WHERE tree_code = '{InputCode.Text}'"
+            cmd = New OdbcCommand(query, Conn)
+            Rd = cmd.ExecuteReader
+            Rd.Read()
+
+            If Rd.HasRows Then
+                InputName.Text = Rd.Item("name")
+                InputCode.Text = Rd.Item("tree_code")
+                InputSciName.Text = Rd.Item("scientific_name")
+                InputTreeAmount.Text = Rd.Item("tree_amount")
+            Else
+                MsgBox("Data tidak ditemukan", MsgBoxStyle.Exclamation, "Error")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Error")
+        End Try
+    End Sub
+
+    Private Sub EditTree()
+        Try
+            Dim query = $"UPDATE mytrees SET name = '{InputName.Text}', scientific_name = '{InputSciName.Text}', tree_code = '{InputCode.Text}', tree_amount = '{InputTreeAmount.Text}'"
+            cmd = New OdbcCommand(query, Conn)
+            cmd.ExecuteNonQuery()
+            MsgBox("Data Berhasil Diubah", MsgBoxStyle.Information, "Sukses")
+            ClearData()
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Error")
+        End Try
+    End Sub
+
+    Private Sub RemoveTree()
+        Try
+            Dim confirm = MessageBox.Show("Apa Anda yakin menghapusnya?", "Peringatan", MessageBoxButtons.YesNo)
+
+            If confirm = DialogResult.Yes Then
+                Dim query = $"DELETE FROM mytrees WHERE tree_code = '{InputCode.Text}'"
+                cmd = New OdbcCommand(query, Conn)
+                cmd.ExecuteNonQuery()
+                MsgBox("Data Berhasil Dihapus", MsgBoxStyle.Information, "Sukses")
+                ClearData()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Error")
+        End Try
+    End Sub
+
 
     Private Sub AddButton_Click(sender As Object, e As EventArgs) Handles AddButton.Click
 
@@ -36,9 +84,33 @@ Public Class AdminForm
 
     End Sub
 
+
+
     Private Sub InputTreeAmount_KeyPress(sender As Object, e As KeyPressEventArgs) Handles InputTreeAmount.KeyPress
         If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
             e.Handled = True 'Beritahu sistem bahwa event sudah ditangani
+        End If
+    End Sub
+
+    Private Sub EditButton_Click(sender As Object, e As EventArgs) Handles EditButton.Click
+        If InputCode.Text = "" Or InputName.Text = "" Or InputSciName.Text = "" Or InputTreeAmount.Text = "" Then
+            MsgBox("Harap isi semua field", MsgBoxStyle.Critical, "Error")
+        Else
+            EditTree()
+        End If
+    End Sub
+
+    Private Sub InputCode_KeyPress(sender As Object, e As KeyPressEventArgs) Handles InputCode.KeyPress
+        If e.KeyChar = Chr(13) Then
+            SearchTree()
+        End If
+    End Sub
+
+    Private Sub DeleteButton_Click(sender As Object, e As EventArgs) Handles DeleteButton.Click
+        If InputCode.Text = "" Or InputName.Text = "" Or InputSciName.Text = "" Or InputTreeAmount.Text = "" Then
+            MsgBox("Harap isi semua field", MsgBoxStyle.Critical, "Error")
+        Else
+            RemoveTree()
         End If
     End Sub
 End Class
